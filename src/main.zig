d265ceab877d79c356c56aeda6c7042b98dab309
sh0jitmy <shjtmy@gmail.com>
2026-04-03 00:02:05 +0900

    change stdout

--- src/main.zig
+++ src/main.zig
@@ -3,15 +3,17 @@ const lib = @import("lib.zig");
 const version = @import("version.zig").version;
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
+    var outbuf: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&outbuf);
+    const stdout = &stdout_writer.interface;
 
-    try stdout.print("Hello, {s}!\n", .{"Zig CI/CD"});
+    _ = try stdout.print("Hello, {s}!\n", .{"Zig CI/CD"});
 
     const result = lib.add(2, 3);
-    try stdout.print("2 + 3 = {d}\n", .{result});
+    _ = try stdout.print("2 + 3 = {d}\n", .{result});
 
     const fizz = lib.fizzBuzz(15);
-    try stdout.print("fizzBuzz(15) = {s}\n", .{fizz});
+    _ = try stdout.print("fizzBuzz(15) = {s}\n", .{fizz});
 
-    try stdout.print("version = {s}\n", .{version});
+    _ = try stdout.print("version = {s}\n", .{version});
 }
